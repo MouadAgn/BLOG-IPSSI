160a6e0215eaee947ad085fcab10d0996d0ae66b
Skylox1201 <60779046+Skylox1201@users.noreply.github.com>
2023-09-27 17:42:57 +0200

    Ajout des liens à la navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,25 +1,28 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">MyBlog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">MyBlog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarScroll">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Blogs</a>
+                    <a class="nav-link active" aria-current="page" href="liste_blog.php">Blogs</a>
                 </li>
                 <li>
-                    <a class="nav-link active" aria-current="page" href="#">Mes Blogs</a>
+                    <a class="nav-link active" aria-current="page" href="myArticles.php">Mes Blogs</a>
+                </li>
+                <li>
+                    <a class="nav-link active" aria-current="page" href="blog.php">Ecrire un article</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Profil
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Inscription</a></li>
-                    <li><a class="dropdown-item" href="#">Connexion</a></li>
-                </ul>
+                        Profil
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Inscription</a></li>
+                        <li><a class="dropdown-item" href="#">Connexion</a></li>
+                    </ul>
                 </li>
             </ul>
             <form class="d-flex" role="search">
@@ -27,11 +30,6 @@
                 <button class="btn btn-outline-success" type="submit"><i class="fas fa-search"></i></button>
                 <a class="btn btn-logout" href="actions/logoutAction.php">LogOut</a>
             </form>
-            </div>
         </div>
-    </nav>
-
-
-
-
-
+    </div>
+</nav>
