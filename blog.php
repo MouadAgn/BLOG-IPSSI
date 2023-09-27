2e7fb767fe69cc3fae04045436ebeb6e2d9a0dea
Skylox1201 <60779046+Skylox1201@users.noreply.github.com>
2023-09-27 16:11:25 +0200

    Requete permettant d'enregistrer un nouvel article

--- blog.php
+++ blog.php
@@ -1,13 +1,9 @@
+<?php require ('actions/articles/saveArticles.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<?php include 'includes/head.php'; ?>
 
-</head>
 </head>
 
 <body>
@@ -16,8 +12,14 @@
             <div class="col-md-12">
                 <h1>Saisie d'article</h1>
                 <hr>
+                <?php if(isset($error_msg))  {  echo '<p style=color:red;>'.$error_msg.'</p>';   }  ?>
                 <form action="" method="post">
-                    <div class="form-group row">
+                <div class="form-group row">
+                        <div class="col-md-12">
+                        <input type="text" class="form-control" placeholder="Titre" name="title">
+                        </div>
+                    </div>
+                    <div class="form-group row mt-2">
                         <div class="col-md-12">
                             <textarea class="form-control" id="editor" name="content"></textarea>
                             <script src="./ckeditor/ckeditor.js"></script>
@@ -31,8 +33,8 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <div class="d-grid gap-2 col-md-8 mt-2">
-                            <button class="btn btn-primary" type="submit">Submit</button>
+                        <div class="d-grid gap-2 col-md-12 mt-2">
+                            <button class="btn btn-primary" type="submit" name="save_article">Submit</button>
                         </div>
                     </div>
                 </form>
