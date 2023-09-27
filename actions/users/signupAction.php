5a2d981580f0ccc9a5c22d7d17cc4f28759cb7fb
MouadAgn <aoughanemouad1@gmail.com>
2023-09-27 15:26:15 +0200

    SignUp System
    SignUp System

---
+++ actions/users/signupAction.php
@@ -0,0 +1,57 @@
+<?php 
+    session_start();
+    require ('actions/database.php');
+
+    // Validate the FORM
+    if(isset($_POST['validate']))
+    {
+        //Check if the user filled the form.
+        if(!empty($_POST['pseudo']) AND !empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['email']) AND !empty($_POST['mdp']))
+        {
+            //User's DATAS.
+            $user_pseudo = htmlspecialchars($_POST['pseudo']);
+            $user_nom = htmlspecialchars($_POST['nom']);
+            $user_prenom = htmlspecialchars($_POST['prenom']);
+            $user_email = htmlspecialchars($_POST['email']);
+            $user_password = password_hash($_POST['mdp'] , PASSWORD_DEFAULT);
+
+            //Check if the user already exists on the websit
+            $checkIfUserAlreadyExists = $bdd->prepare('SELECT pseudo from user where pseudo = ?');
+            $checkIfUserAlreadyExists->execute(array($user_pseudo));
+            
+
+            if($checkIfUserAlreadyExists->rowCount() == 0)
+            {
+                //Insert the user on the database
+                $InsertUserOnWebsite = $bdd ->prepare('INSERT INTO user(pseudo, nom, prenom, email, mdp) VALUES(?, ?, ?, ?, ?)');
+                $InsertUserOnWebsite->execute(array($user_pseudo, $user_nom, $user_prenom, $user_email, $user_password));
+
+                //Get user's informations.
+                $GetInfosOfThisUser = $bdd ->prepare('SELECT id, pseudo, nom, prenom, email, mdp FROM user WHERE nom = ? AND prenom = ? AND pseudo = ?');
+                $GetInfosOfThisUser->execute(array($user_nom, $user_prenom, $user_pseudo));
+
+                $usersInfos = $GetInfosOfThisUser->fetch();
+
+                $_SESSION['auth'] = true;
+                $_SESSION['id'] = $usersInfos['id'];
+                $_SESSION['nom'] = $usersInfos['nom'];
+                $_SESSION['prenom'] = $usersInfos['prenom'];
+                $_SESSION['pseudo'] = $usersInfos['pseudo'];
+
+                header("Location: indexx.php");
+
+            }
+            else
+            {
+                $error_msg = "L'utilisateur existe déja sur le site";
+            }
+
+
+        }
+        else
+        {
+            $error_msg = "Veuillez completer tous les champs...";
+        }
+    }
+
+?>
---
+++ signup.php
@@ -0,0 +1,40 @@
+<?php require ('actions/users/signupAction.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'includes/head.php'; ?>
+
+<body >
+
+    <br><br><br>
+    <form class="container" method="POST" action="">
+
+        <?php if(isset($error_msg))  {  echo '<p style=color:red;>'.$error_msg.'</p>';   }  ?>
+
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+            <input type="text" class="form-control" name="pseudo">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nom</label>
+            <input type="text" class="form-control" name="nom">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Prenom</label>
+            <input type="text" class="form-control" name="prenom">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="text" class="form-control" name="email">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" name="mdp">
+        </div>
+        <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
+        <br><br>
+        <a href="login.php"><p>J'ai déja un compte, je me connecte</p></a>
+    </form>
+    
+</body>
+</html>
