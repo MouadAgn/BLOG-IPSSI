a5dc5fd405a96bc3934e8663330ffb2ac990eb59
MouadAgn <aoughanemouad1@gmail.com>
2023-09-28 09:49:43 +0200

    Update NavBar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">MyBlog</a>
@@ -24,3 +25,53 @@
         </div>
     </div>
 </nav>
+=======
+<?php require('actions/database.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navbar</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Mes Articles</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Les Articles</a>
+            </li>
+            <li>
+                    <a class="nav-link active" aria-current="page" href="blog.php">Ecrire un article</a>
+            </li>
+        </ul>
+        <span class="navbar-text mr-3">
+            Hello, <?php echo $_SESSION['pseudo']; ?>
+        </span>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <a class="btn btn-danger ml-3" href="actions/logoutAction.php">LogOut</a>
+    </div>
+</nav>
+
+<!-- Optional: jQuery library, Popper.js, and Bootstrap JS for some Bootstrap components -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+>>>>>>> Stashed changes
