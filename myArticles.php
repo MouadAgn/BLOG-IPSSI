28b2b80ea908d7c2cd6b5f248f51b82803cc860d
Skylox1201 <60779046+Skylox1201@users.noreply.github.com>
2023-09-28 09:31:46 +0200

    Afficher un article si l utilisateur n a pas ecrit d article

--- myArticles.php
+++ myArticles.php
@@ -10,15 +10,21 @@
     <?php include 'includes/navbar.php'; ?>
     <div class="container">
         <div class="row">
-            <?php foreach ($user_articles as $article) { ?>
-                <div class="card mt-3 ms-3 col-4" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $article["titre"] ?></h5>
-                        <p class="card-text"><?php echo substr(strip_tags($article["contenue"]), 0, 100) ?></p>
-                        <a href="#" class="card-link">Lire l'article</a>
-                        <a href="#" class="card-link">Modifier l'article</a>
+            <?php
+            if (sizeof($user_articles) > 0) {
+                foreach ($user_articles as $article) { ?>
+                    <div class="card mt-3 ms-3 col-4" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $article["titre"] ?></h5>
+                            <p class="card-text"><?php echo substr(strip_tags($article["contenue"]), 0, 100) ?></p>
+                            <a href="#" class="card-link">Lire l'article</a>
+                            <a href="#" class="card-link">Modifier l'article</a>
+                        </div>
                     </div>
-                </div>
+                <?php }
+            } else { ?>
+                <p class="fs-1">Vous n'avez écrit aucun article</p>
+                <a href="blog.php">Ecrire un article</a>
             <?php } ?>
         </div>
     </div>
